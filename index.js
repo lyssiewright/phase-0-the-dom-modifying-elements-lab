99d49a382995f0e20727255045a540e400b5f781
Lyssie Wright <lyssiewright.15@gmail.com>
2022-01-19 22:04:57 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const elem = document.querySelector('main');
+elem.parentNode.removeChild(elem);
+
+const newHeader = document.createElement("H1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+
+newHeader.innerHTML = 
+"<h1>Lyssie Wright is the champion</h1";
